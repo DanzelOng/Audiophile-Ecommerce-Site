a183c0a897e37ce5537128399eb14be167b28c7b
danzel ong <danzelongwj01@gmail.com>
2024-04-23 19:47:35 +0800

    feat (routing): integrated react-router for csr, framer-motion for route animation with application

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import App from './components/App';
+import './scss/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AnimatePresence>
+        <App />
+      </AnimatePresence>
+    </Router>
   </React.StrictMode>
 );
