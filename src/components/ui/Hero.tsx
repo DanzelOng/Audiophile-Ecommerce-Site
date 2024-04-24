e1bff9649bdac6fce6de2acc3f27e274e093f1b3
danzel ong <danzelongwj01@gmail.com>
2024-04-24 15:18:08 +0800

    feat (header-hero): implemented the hero sub component of header

---
+++ src/components/ui/Hero.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { heroImgs } from '../../data/data';
+import ResponsiveImage from './ResponsiveImage';
+import Button from './Button';
+
+function Hero() {
+  return (
+    <section className='header__hero'>
+      <motion.div
+        className='header__hero-img-container'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.1, delay: 0.7 }}
+      >
+        <ResponsiveImage
+          className='header__hero-img'
+          images={heroImgs}
+          title='XX99 Mark II headphones'
+        />
+      </motion.div>
+      <motion.div
+        className='header__hero-content'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.1, delay: 1.2 }}
+      >
+        <span className='header__hero-subheading'>New Product</span>
+        <h1 className='header__hero-heading'>XX99 Mark II Headphones</h1>
+        <p className='header__hero-info'>
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </p>
+        <Button type='regular'>
+          <Link to='/headphones/xx99-mark-II-headphones'>See Product</Link>
+        </Button>
+      </motion.div>
+    </section>
+  );
+}
+
+export default Hero;
