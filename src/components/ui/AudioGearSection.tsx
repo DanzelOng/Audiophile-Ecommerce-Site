934a8cc71f474ced4d84830e86bb9f6f9b0bbe8a
danzel ong <danzelongwj01@gmail.com>
2024-04-25 13:48:00 +0800

    feat (audio-sect): implemented audio gear component

--- src/components/ui/AudioGearSection.tsx
+++ src/components/ui/AudioGearSection.tsx
@@ -1,4 +1,31 @@
+import ResponsiveImage from './ResponsiveImage';
+import { audioGearImgs } from '../../data/data';
+
 function AudioGearSection() {
-  return <section>Audio gear section</section>;
+  return (
+    <section className='audio-gear | container'>
+      <div className='audio-gear__img-container'>
+        <ResponsiveImage
+          className='audio-gear__img'
+          images={audioGearImgs}
+          title='Test'
+        />
+      </div>
+      <div className='audio-gear__content'>
+        <h3 className='audio-gear__heading'>
+          Bringing you the <span>best</span> audio gear
+        </h3>
+        <p className='audio-gear__info'>
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+      </div>
+    </section>
+  );
 }
+
 export default AudioGearSection;
