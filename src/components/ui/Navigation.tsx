d310843e273dacc6c3dd077f8d7583c1325fb8e5
danzel ong <danzelongwj01@gmail.com>
2024-04-26 00:09:53 +0800

    feat (navigation): implemented a reusable navigation component

---
+++ src/components/ui/Navigation.tsx
@@ -0,0 +1,56 @@
+import { Link } from 'react-router-dom';
+import { categoryNavigations } from '../../data/data';
+
+type CategoryNavigatorProps = {
+  dataAttrName: string;
+  dataAttrValue: string;
+  ariaHiddenStatus?: boolean;
+};
+
+type DataProps = {
+  [key: string]: string;
+};
+
+const root = document.getElementById('root') as HTMLDivElement;
+
+function Navigation({
+  dataAttrName,
+  dataAttrValue,
+  ariaHiddenStatus,
+}: CategoryNavigatorProps) {
+  const dataProps = {} as DataProps;
+  dataProps[`data-${dataAttrName}`] = dataAttrValue;
+  const ariaHidden =
+    ariaHiddenStatus !== undefined ? !ariaHiddenStatus : undefined;
+
+  return (
+    <section className='audio-products' {...dataProps} aria-hidden={ariaHidden}>
+      {categoryNavigations.map((category, index) => (
+        <div key={index} className={`audio-products__${category.category}`}>
+          <img
+            className='audio-products__image'
+            src={category.src}
+            alt={category.category}
+          />
+          <div className={`audio-products__content`}>
+            <span>{category.category}</span>
+            <Link
+              to={category.url}
+              className='audio-products__link'
+              onClick={() => {
+                if (root.hasAttribute('data-overlay')) {
+                  root.removeAttribute('data-overlay');
+                }
+              }}
+            >
+              Shop
+              <img className='audio-products_link-image' src={category.arrow} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+}
+
+export default Navigation;
