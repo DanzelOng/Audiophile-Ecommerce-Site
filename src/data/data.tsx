753f9e5dfa795c3f53b1450ff3aeba4ed3498fb4
danzel ong <danzelongwj01@gmail.com>
2024-04-28 01:52:51 +0800

    chore (data): updated data for dynamically rendering a product

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -592,3 +592,510 @@ export const headphones: Products[] = [
     ],
   },
 ];
+
+export const speakers: Products[] = [
+  {
+    id: 1,
+    slug: 'zx9-speaker',
+    name: 'ZX9 Speaker',
+    new: true,
+    price: 4500,
+    description:
+      'Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.',
+    features:
+      'Connect via Bluetooth or nearly any wired source. This speaker features optical, digital coaxial, USB Type-B, stereo RCA, and stereo XLR inputs, allowing you to have up to five wired source devices connected for easy switching. Improved bluetooth technology offers near lossless audio quality at up to 328ft (100m).\nDiscover clear, more natural sounding highs than the competition with ZX9’s signature planar diaphragm tweeter. Equally important is its powerful room-shaking bass courtesy of a 6.5” aluminum alloy bass unit. You’ll be able to enjoy equal sound quality whether in a large room or small den. Furthermore, you will experience new sensations from old songs since it can respond to even the subtle waveforms.',
+    includes: [
+      {
+        quantity: 2,
+        item: 'Speaker unit',
+      },
+      {
+        quantity: 2,
+        item: 'Speaker cloth panel',
+      },
+      {
+        quantity: 1,
+        item: 'User manual',
+      },
+      {
+        quantity: 1,
+        item: '3.5mm 10m audio cable',
+      },
+      {
+        quantity: 1,
+        item: '10m optical cable',
+      },
+    ],
+    images: [
+      {
+        path: 'assets/product-zx9-speaker/mobile/image-category-page-preview.jpg',
+        width: mobileBp,
+      },
+      {
+        path: 'assets/product-zx9-speaker/tablet/image-category-page-preview.jpg',
+        width: tabletBp,
+      },
+      {
+        path: 'assets/product-zx9-speaker/desktop/image-category-page-preview.jpg',
+      },
+    ],
+    preview: [
+      {
+        path: '/assets/product-zx9-speaker/mobile/image-category-page-preview.jpg',
+        width: mobileBp,
+      },
+      {
+        path: '/assets/product-zx9-speaker/tablet/image-category-page-preview.jpg',
+        width: tabletBp,
+      },
+      {
+        path: '/assets/product-zx9-speaker/desktop/image-category-page-preview.jpg',
+      },
+    ],
+    gallery: [
+      {
+        attribute: 'first',
+        images: [
+          {
+            path: '/assets/product-zx9-speaker/mobile/image-gallery-1.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-zx9-speaker/tablet/image-gallery-1.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/product-zx9-speaker/desktop/image-gallery-1.jpg',
+          },
+        ],
+      },
+      {
+        attribute: 'second',
+        images: [
+          {
+            path: '/assets/product-zx9-speaker/mobile/image-gallery-2.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-zx9-speaker/tablet/image-gallery-2.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-zx9-speaker/desktop/image-gallery-2.jpg',
+          },
+        ],
+      },
+      {
+        attribute: 'third',
+        images: [
+          {
+            path: '/assets/product-zx9-speaker/mobile/image-gallery-3.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-zx9-speaker/tablet/image-gallery-3.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/product-zx9-speaker/desktop/image-gallery-3.jpg',
+          },
+        ],
+      },
+    ],
+    other: [
+      {
+        slug: 'zx7-speaker',
+        name: 'ZX7 Speaker',
+        category: 'speakers',
+        images: [
+          {
+            path: '/assets/shared/mobile/image-zx7-speaker.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/shared/tablet/image-zx7-speaker.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/shared/desktop/image-zx7-speaker.jpg',
+          },
+        ],
+      },
+      {
+        slug: 'xx99-mark-one-headphones',
+        name: 'XX99 Mark I',
+        category: 'headphones',
+        images: [
+          {
+            path: '/assets/shared/mobile/image-xx99-mark-one-headphones.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/shared/tablet/image-xx99-mark-one-headphones.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/shared/desktop/image-xx99-mark-one-headphones.jpg',
+          },
+        ],
+      },
+      {
+        slug: 'xx59-headphones',
+        name: 'XX59',
+        category: 'headphones',
+        images: [
+          {
+            path: '/assets/shared/mobile/image-xx59-headphones.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/shared/tablet/image-xx59-headphones.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/shared/desktop/image-xx59-headphones.jpg',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    slug: 'zx7-speaker',
+    name: 'ZX7 Speaker',
+    new: false,
+    price: 3500,
+    description:
+      'Stream high quality sound wirelessly with minimal loss. The ZX7 bookshelf speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.',
+    features:
+      'Reap the advantages of a flat diaphragm tweeter cone. This provides a fast response rate and excellent high frequencies that lower tiered bookshelf speakers cannot provide. The woofers are made from aluminum that produces a unique and clear sound. XLR inputs allow you to connect to a mixer for more advanced usage.\nThe ZX7 speaker is the perfect blend of stylish design and high performance. It houses an encased MDF wooden enclosure which minimises acoustic resonance. Dual connectivity allows pairing through bluetooth or traditional optical and RCA input. Switch input sources and control volume at your finger tips with the included wireless remote. This versatile speaker is equipped to deliver an authentic listening experience.',
+    includes: [
+      {
+        quantity: 2,
+        item: 'Speaker unit',
+      },
+      {
+        quantity: 2,
+        item: 'Speaker cloth panel',
+      },
+      {
+        quantity: 1,
+        item: 'User manual',
+      },
+      {
+        quantity: 1,
+        item: '3.5mm 7.5m audio cable',
+      },
+      {
+        quantity: 1,
+        item: '7.5m optical cable',
+      },
+    ],
+    images: [
+      {
+        path: 'assets/product-zx7-speaker/mobile/image-category-page-preview.jpg',
+        width: mobileBp,
+      },
+      {
+        path: 'assets/proYX1 Wireless Earphones/image-category-page-preview.jpg',
+        width: tabletBp,
+      },
+      {
+        path: 'assets/product-zx7-speaker/desktop/image-category-page-preview.jpg',
+      },
+    ],
+    preview: [
+      {
+        path: '/assets/product-zx7-speaker/mobile/image-category-page-preview.jpg',
+        width: mobileBp,
+      },
+      {
+        path: '/assets/product-zx7-speaker/tablet/image-category-page-preview.jpg',
+        width: tabletBp,
+      },
+      {
+        path: '/assets/product-zx7-speaker/desktop/image-category-page-preview.jpg',
+      },
+    ],
+    gallery: [
+      {
+        attribute: 'first',
+        images: [
+          {
+            path: '/assets/product-7-speaker/mobile/image-gallery-1.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-zx7-speaker/tablet/image-gallery-1.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/product-zx7-speaker/desktop/image-gallery-1.jpg',
+          },
+        ],
+      },
+      {
+        attribute: 'second',
+        images: [
+          {
+            path: '/assets/product-zx7-speaker/mobile/image-gallery-2.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-zx7-speaker/tablet/image-gallery-2.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-zx7-speaker/desktop/image-gallery-2.jpg',
+          },
+        ],
+      },
+      {
+        attribute: 'third',
+        images: [
+          {
+            path: '/assets/product-zx7-speaker/mobile/image-gallery-3.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-zx7-speaker/tablet/image-gallery-3.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/product-zx7-speaker/desktop/image-gallery-3.jpg',
+          },
+        ],
+      },
+    ],
+    other: [
+      {
+        slug: 'zx9-speaker',
+        name: 'ZX9 Speaker',
+        category: 'speakers',
+        images: [
+          {
+            path: '/assets/shared/mobile/image-zx9-speaker.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/shared/tablet/image-zx9-speaker.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/shared/desktop/image-zx9-speaker.jpg',
+          },
+        ],
+      },
+      {
+        slug: 'xx99-mark-one-headphones',
+        name: 'XX99 Mark I',
+        category: 'headphones',
+        images: [
+          {
+            path: '/assets/shared/mobile/image-xx99-mark-one-headphones.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/shared/tablet/image-xx99-mark-one-headphones.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/shared/desktop/image-xx99-mark-one-headphones.jpg',
+          },
+        ],
+      },
+      {
+        slug: 'xx59-headphones',
+        name: 'XX59',
+        category: 'headphones',
+        images: [
+          {
+            path: '/assets/shared/mobile/image-xx59-headphones.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/shared/tablet/image-xx59-headphones.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/shared/desktop/image-xx59-headphones.jpg',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const earphones: Products[] = [
+  {
+    id: 1,
+    slug: 'yx1-wireless-earphones',
+    name: 'YX1 Wireless Earphones',
+    new: true,
+    price: 599,
+    description:
+      'Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.',
+    features:
+      'Experience unrivalled stereo sound thanks to innovative acoustic technology. With improved ergonomics designed for full day wearing, these revolutionary earphones have been finely crafted to provide you with the perfect fit, delivering complete comfort all day long while enjoying exceptional noise isolation and truly immersive sound.\nThe YX1 Wireless Earphones features customizable controls for volume, music, calls, and voice assistants built into both earbuds. The new 7-hour battery life can be extended up to 28 hours with the charging case, giving you uninterrupted play time. Exquisite craftsmanship with a splash resistant design now available in an all new white and grey color scheme as well as the popular classic black.',
+    includes: [
+      {
+        quantity: 2,
+        item: 'Earphone unit',
+      },
+      {
+        quantity: 6,
+        item: 'Multi-size earplugs',
+      },
+      {
+        quantity: 1,
+        item: 'User manual',
+      },
+      {
+        quantity: 1,
+        item: 'USB-C charging cable',
+      },
+      {
+        quantity: 1,
+        item: 'Travel pouch',
+      },
+    ],
+    images: [
+      {
+        path: 'assets/product-yx1-earphones/mobile/image-category-page-preview.jpg',
+        width: mobileBp,
+      },
+      {
+        path: 'assets/product-yx1-earphones/tablet/image-category-page-preview.jpg',
+        width: tabletBp,
+      },
+      {
+        path: 'assets/product-yx1-earphones/desktop/image-category-page-preview.jpg',
+      },
+    ],
+    preview: [
+      {
+        path: '/assets/product-yx1-earphones/mobile/image-category-page-preview.jpg',
+        width: mobileBp,
+      },
+      {
+        path: '/assets/product-yx1-earphones/tablet/image-category-page-preview.jpg',
+        width: tabletBp,
+      },
+      {
+        path: '/assets/product-yx1-earphones/desktop/image-category-page-preview.jpg',
+      },
+    ],
+    gallery: [
+      {
+        attribute: 'first',
+        images: [
+          {
+            path: '/assets/product-yx1-earphones/mobile/image-gallery-1.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-yx1-earphones/tablet/image-gallery-1.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/product-yx1-earphones/desktop/image-gallery-1.jpg',
+          },
+        ],
+      },
+      {
+        attribute: 'second',
+        images: [
+          {
+            path: '/assets/product-yx1-earphones/mobile/image-gallery-2.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-yx1-earphones/tablet/image-gallery-2.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-yx1-earphones/desktop/image-gallery-2.jpg',
+          },
+        ],
+      },
+      {
+        attribute: 'third',
+        images: [
+          {
+            path: '/assets/product-yx1-earphones/mobile/image-gallery-3.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/product-yx1-earphones/tablet/image-gallery-3.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/product-yx1-earphones/desktop/image-gallery-3.jpg',
+          },
+        ],
+      },
+    ],
+    other: [
+      {
+        slug: 'xx99-mark-one-headphones',
+        name: 'XX99 Mark I',
+        category: 'headphones',
+        images: [
+          {
+            path: '/assets/shared/mobile/image-xx99-mark-one-headphones.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/shared/tablet/image-xx99-mark-one-headphones.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/shared/desktop/image-xx99-mark-one-headphones.jpg',
+          },
+        ],
+      },
+      {
+        slug: 'xx59-headphones',
+        name: 'XX59',
+        category: 'headphones',
+        images: [
+          {
+            path: '/assets/shared/mobile/image-xx59-headphones.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/shared/tablet/image-xx59-headphones.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/shared/desktop/image-xx59-headphones.jpg',
+          },
+        ],
+      },
+      {
+        slug: 'zx9-speaker',
+        name: 'ZX9 Speaker',
+        category: 'speakers',
+        images: [
+          {
+            path: '/assets/shared/mobile/image-zx9-speaker.jpg',
+            width: mobileBp,
+          },
+          {
+            path: '/assets/shared/tablet/image-zx9-speaker.jpg',
+            width: tabletBp,
+          },
+          {
+            path: '/assets/shared/desktop/image-zx9-speaker.jpg',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const getProducts = {
+  'headphones': headphones,
+  'speakers': speakers,
+  'earphones': earphones,
+};
