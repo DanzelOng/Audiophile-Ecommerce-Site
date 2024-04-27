086a12de8101276253f4ce5efc45e4a9798e57f3
danzel ong <danzelongwj01@gmail.com>
2024-04-28 00:27:06 +0800

    Revert "revert "feat(route-headphones): implemented the headphone component for headphone route""
    This reverts commit 5212e2a907218ca78abcae528a5bfdc3361ccfb7.

--- src/pages/Headphones.tsx
+++ src/pages/Headphones.tsx
@@ -1,12 +1,42 @@
-import { Outlet } from 'react-router-dom';
+import { headphones } from '../data/data';
 import Main from '../layout/Main';
+import Button from '../components/ui/Button';
+import ResponsiveImage from '../components/ui/ResponsiveImage';
+import Navigation from '../components/ui/Navigation';
 
 function Headphones() {
   return (
     <Main className='pt-10'>
-      Headphones
-      <Outlet />
+      <ul
+        className='headphone-products'
+        role='list'
+        aria-label='Headphone products'
+      >
+        {headphones.map((headphone) => (
+          <li className='headphone' key={headphone.id}>
+            <div className='headphone__img-container'>
+              <ResponsiveImage
+                className='headphone__img'
+                images={headphone.preview}
+                title={headphone.name}
+              />
+            </div>
+            <div className='headphone__content'>
+              {headphone.id === 1 && (
+                <span className='headphone__subheading'>New Product</span>
+              )}
+              <h3 className='headphone__heading'>{headphone.name}</h3>
+              <p className='headphone__description'>{headphone.description}</p>
+              <Button type='regular' href={headphone.slug}>
+                See Product
+              </Button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <Navigation dataAttrName='navigation-mode' dataAttrValue='desktop' />
     </Main>
   );
 }
+
 export default Headphones;
