37c4ef3409690a1233ebdcb97b864480765249cb
danzel ong <danzelongwj01@gmail.com>
2024-05-03 15:48:20 +0800

    feat (page not found): implemented component to display when invalid route match

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,12 +1,38 @@
-import { Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import Main from '../layout/Main';
+import Navigation from '../components/ui/Navigation';
+import Button from '../components/ui/Button';
 
 function PageNotFound() {
+  const navigate = useNavigate();
   return (
-    <Main>
-      Page not found
-      <Outlet />
+    <Main className='mt-5'>
+      <Helmet>
+        <title>Page Not Found</title>
+      </Helmet>
+      <div className='pageNotFound'>
+        <div className='pageNotFound__wrapper | container'>
+          <Button type='navigation' onClick={() => navigate('/')}>
+            Back to Home &#8592;
+          </Button>
+          <div className='pageNotFound__text'>
+            <p> Sorry, we can't find the page you're looking for</p>
+            <p>Let's get you back on track...</p>
+          </div>
+          <div className='pageNotFound__empty-navigation'>
+            <h6 className='pageNotFound__empty-heading'>
+              Check Out Our Products
+            </h6>
+            <Navigation
+              dataAttrName='navigation-mode'
+              dataAttrValue='desktop'
+            />
+          </div>
+        </div>
+      </div>
     </Main>
   );
 }
+
 export default PageNotFound;
